cee6bf54209d9c298d0ad96ad0fa3af6a13fefe8
gagagahuy <gagagahuy@gmail.com>
2023-02-21 08:06:53 +0700

    upload source code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Home, Login, Public } from "./containers/public/";
+import { Routes, Route } from "react-router-dom";
+import path from "./ultis/path";
 function App() {
-    return <div className="App"></div>;
+    const { test } = useSelector((state) => state.app);
+
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={path.PUBLIC} element={<Public />}>
+                    <Route path={path.HOME} element={<Home />} />
+                    <Route path={path.LOGIN} element={<Login />} />
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/containers/public/Public.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { SidebarLeft, SidebarRight } from "../../components";
+const Public = () => {
+    return (
+        <div className="w-full flex">
+            <div className="w-[240px] flex-none">
+                <SidebarLeft />
+            </div>
+            <div className="flex-auto">
+                <Outlet />
+            </div>
+            <div className="w-[329px] flex-none">
+                <SidebarRight />
+            </div>
+        </div>
+    );
+};
+
+export default Public;
